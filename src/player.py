a6c3fe36316c6bf58ec81faf19d193653d4ff718
Chris <tsudonymn@gmail.com>
2023-06-08 23:32:37 -0400

    New rover

--- src/player.py
+++ src/player.py
@@ -20,7 +20,7 @@ class Player(pygame.sprite.Sprite):
         width = 40
         height = 60
         # self.image = pygame.Surface([width, height])
-        IMAGE = pygame.image.load('robot.png').convert_alpha()
+        IMAGE = pygame.image.load('assets/rover.png').convert_alpha()
         self.image = IMAGE
         # self.image.fill(RED)
 
@@ -43,31 +43,31 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += self.change_x
 
         # See if we hit anything
-        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
-        for block in block_hit_list:
-            # If we are moving right,
-            # set our right side to the left side of the item we hit
-            if self.change_x > 0:
-                self.rect.right = block.rect.left
-            elif self.change_x < 0:
-                # Otherwise if we are moving left, do the opposite.
-                self.rect.left = block.rect.right
+        # block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        # for block in block_hit_list:
+        #     # If we are moving right,
+        #     # set our right side to the left side of the item we hit
+        #     if self.change_x > 0:
+        #         self.rect.right = block.rect.left
+        #     elif self.change_x < 0:
+        #         # Otherwise if we are moving left, do the opposite.
+        #         self.rect.left = block.rect.right
 
         # Move up/down
         self.rect.y += self.change_y
 
         # Check and see if we hit anything
-        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
-        for block in block_hit_list:
-
-            # Reset our position based on the top/bottom of the object.
-            if self.change_y > 0:
-                self.rect.bottom = block.rect.top
-            elif self.change_y < 0:
-                self.rect.top = block.rect.bottom
-
-            # Stop our vertical movement
-            self.change_y = 0
+        # block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        # for block in block_hit_list:
+        #
+        #     # Reset our position based on the top/bottom of the object.
+        #     if self.change_y > 0:
+        #         self.rect.bottom = block.rect.top
+        #     elif self.change_y < 0:
+        #         self.rect.top = block.rect.bottom
+        #
+        #     # Stop our vertical movement
+        #     self.change_y = 0
 
     def calc_grav(self):
         """ Calculate effect of gravity. """
